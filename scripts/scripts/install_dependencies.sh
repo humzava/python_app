1d5321272e89787e01bebc6da7f4f4f1641a3906
humzava <humzafidava@gmail.com>
2024-10-07 06:58:05 +0500

    Delete scripts/scripts directory

--- scripts/scripts/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo systemctl stop python_app.service
-cd /home/ubuntu/python_app  # Change this to your application's directory
-sudo apt install python3-pip -y
-sudo apt install python3-venv
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r /home/ubuntu/python_app/requirements.txt
-exit
