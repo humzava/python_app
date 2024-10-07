43c5a08e69274850ce8d16e483799fd84dfbbdeb
humzava <humzafidava@gmail.com>
2024-10-07 06:57:14 +0500

    Delete install_dependencies.sh

--- install_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo systemctl stop myapp.service
-sudo apt install python3-pip -y
-sudo apt install python3-venv
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r /home/ubuntu/python-test/requirements.txt
-exit
