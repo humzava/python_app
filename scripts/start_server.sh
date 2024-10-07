20491f2567fe78437b1dfc29f26710bf19f709bd
humzava <humzafidava@gmail.com>
2024-10-07 07:11:21 +0500

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-sudo systemctl start python_app.service
-cd /home/ubuntu/python_app  # Change this to your application's directory
-gunicorn -w 3 -b 0.0.0.0:5000 app:app &  # Start the Flask app using Gunicorn
+sudo systemctl restart python_app.service
