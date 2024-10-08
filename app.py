8a73ac8835c8f4cdbea592d6903683df05a1af2d
humzava <humzafidava@gmail.com>
2024-10-09 00:17:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World after update!'
+    return 'Hello, World. Kis kis ny pipeline bana lee ha?'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
